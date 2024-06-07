9e96f2c3c93e72aa02d44fdd67742e0a82265df6
harianchery <hari.anchery001@gmail.com>
2024-06-07 23:07:10 +0530

    Routing using Navbar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Add from './components/Add';
 import Search from './components/Search';
 import Delete from './components/Delete';
 import View from './components/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      <Add/>
-      <Search/>
-      <Delete/>
-      <View/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Add/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/delete' element={<Delete/>}/>
+      <Route path='/view' element={<View/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
